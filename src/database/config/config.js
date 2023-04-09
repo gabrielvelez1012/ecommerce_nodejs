9c887050fcc973b4b48fdd64f57d8a184aa66c23
gabrielv91 <gabrielvelez10121991@gmail.com>
2023-04-08 21:20:25 -0500

    associations finish

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,12 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "root",
-    "database": "ecommerce_db",
-    "host": "localhost",
-    "dialect": "postgres",
-    "logging":  false
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
